47cc143300bf216c5de7a2fbb45d5c56069c0bab
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2022-11-30 20:39:55 +0700

    Update Program.cs

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,7 +1,8 @@
 ﻿//задача 2.определить чётность числа
-int a = 9;
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
 
-if ( a/2==0 )
+if ( a%2==0 )
 {
 Console.Write("Число чётное");
 }
